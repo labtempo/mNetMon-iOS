0923906ca3b2b86c3b024b4be2e14d64004eef4c
Henrique do Prado Linhares <linhares.h@gmail.com>
2015-12-18 18:35:47 -0200

    melhorando cores do app

---
+++ SignalStrength/AppColors.swift
@@ -0,0 +1,19 @@
+//
+//  AppColors.swift
+//  SignalStrength
+//
+//  Created by Henrique do Prado Linhares on 18/12/15.
+//  Copyright © 2015 Henrique do Prado Linhares. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+
+class AppColors{
+    
+    static let myGreenColor = UIColor(colorLiteralRed: 217/255, green: 255/255, blue: 183/255, alpha: 1)
+    static let myRedColor = UIColor(colorLiteralRed: 255/255, green: 127/255, blue: 127/255, alpha: 1)
+
+
+}
