813d9e3505a023e9b75ee41329e59ad4f602ba7a
Henrique do Prado Linhares <linhares.h@gmail.com>
2017-02-25 23:55:39 -0300

    Atualizando o .gitignore e as cores do app

--- SignalStrength/AppColors.swift
+++ SignalStrength/AppColors.swift
@@ -17,9 +17,9 @@ class AppColors{
     static let viewGreenColor = UIColor(colorLiteralRed: 217/255, green: 255/255, blue: 183/255, alpha: 1)
     static let viewRedColor = UIColor(colorLiteralRed: 255/255, green: 127/255, blue: 127/255, alpha: 1)
     
-    static let signalVeryGoodColor = UIColor(colorLiteralRed: 0/255, green: 255/255, blue: 0/255, alpha: 0.25)
-    static let signalGoodColor = UIColor(colorLiteralRed: 141/255, green: 202/255, blue: 90/255, alpha: 0.25)
-    static let signalRegularColor = UIColor(colorLiteralRed: 203/255, green: 189/255, blue: 86/255, alpha: 0.25)
-    static let signalBadColor = UIColor(colorLiteralRed: 204/255, green: 123/255, blue: 92/255, alpha: 0.25)
-    static let signalVeryBadColor = UIColor(colorLiteralRed: 218/255, green: 71/255, blue: 60/255, alpha: 0.25)
+    static let signalVeryGoodColor = UIColor(colorLiteralRed: 0/255, green: 255/255, blue: 0/255, alpha: 0.3)
+    static let signalGoodColor = UIColor(colorLiteralRed: 141/255, green: 202/255, blue: 90/255, alpha: 0.3)
+    static let signalRegularColor = UIColor(colorLiteralRed: 203/255, green: 189/255, blue: 86/255, alpha: 0.3)
+    static let signalBadColor = UIColor(colorLiteralRed: 204/255, green: 123/255, blue: 92/255, alpha: 0.3)
+    static let signalVeryBadColor = UIColor(colorLiteralRed: 218/255, green: 71/255, blue: 60/255, alpha: 0.3)
 }
