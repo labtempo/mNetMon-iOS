280f71315b8459c8b8d980112bd75f3a1d3ad231
Henrique do Prado Linhares <linhares.h@gmail.com>
2017-04-18 22:29:22 -0300

    Começando a trabalhar na request das leituras para o servidor

---
+++ SignalStrength/ReadService.swift
@@ -0,0 +1,14 @@
+//
+//  ReadService.swift
+//  SignalStrength
+//
+//  Created by Henrique do Prado Linhares on 18/04/17.
+//  Copyright © 2017 Henrique do Prado Linhares. All rights reserved.
+//
+
+import Foundation
+import Alamofire
+
+class ReadService{
+
+}
--- SignalStrength/WorldMapViewController.swift
+++ SignalStrength/WorldMapViewController.swift
@@ -11,19 +11,34 @@ import MapKit
 import RealmSwift
 import Foundation
 
-class WorldMapViewController: UIViewController {
+class WorldMapViewController: UIViewController, MKMapViewDelegate {
+    
+    var updateTimer = NSTimer()
 
+    @IBOutlet weak var mapView: MKMapView!
+    
     override func viewWillAppear(animated: Bool) {
         if !Server.exists() {
             AlertController.presentAlert("Not connected to server", message: "Please, connect to a server")
         }
+        self.updateTimer = NSTimer.scheduledTimerWithTimeInterval(1, target: self, selector: #selector(WorldMapViewController.update), userInfo: nil, repeats: true)
     }
     
     override func viewWillDisappear(animated: Bool) {
+        self.updateTimer.invalidate()
     }
     
     override func viewDidLoad() {
     }
+    
+    func update(){
+        //todo
+        let nePoint = CGPointMake(self.mapView.bounds.origin.x + self.mapView.bounds.size.width, self.mapView.bounds.origin.y)
+        let swPoint = CGPointMake(self.mapView.bounds.origin.x, self.mapView.bounds.origin.y + self.mapView.bounds.size.height)
+        print("NE: \(nePoint)")
+        print("SW: \(swPoint)")
+    }
+    
     
     
 }
